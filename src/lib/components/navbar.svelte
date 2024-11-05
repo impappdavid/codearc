4a56bbfd50e28636b15738ae9a2e2b2fe5319fbd
impappdavid <impappdavid@gmail.com>
2024-11-05 17:29:31 +0100

    Create Navbar component for landing page

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { Link } from "lucide-svelte";
+    import Modetoggle from "./modetoggle.svelte";
+    import Button from "./ui/button/button.svelte";
+</script>
+
+<div class="w-full flex-col justify-center sticky top-0">
+    <div class="flex justify-center">
+    <div
+        class="w-full max-w-7xl py-1 px-12 flex justify-between items-center backdrop-blur	"
+    >
+        
+
+        <div
+            class="w-fit flex justify-center gap-2 h-fit rounded-lg py-1 px-1"
+        >
+        <a
+                href="./"
+                class="w-fit p-1 dark:text-white flex gap-2 font-bold text-lg items-center transition-all"
+            >
+                <Link class="w-6 h-6 " />
+            </a>
+            <a
+                href=""
+                class="hover:bg-zinc-200 dark:hover:bg-zinc-900 flex items-center px-2 py-2 text-sm rounded-lg transition-all font-sans "
+                >Getting started</a
+            >
+
+            <a
+                href=""
+                class="hover:bg-zinc-200 dark:hover:bg-zinc-900 flex items-center px-2 py-2 text-sm rounded-lg transition-all font-sans"
+                >Functions</a
+            >
+            <a
+                href=""
+                class="hover:bg-zinc-200 dark:hover:bg-zinc-900 flex items-center px-2 py-2 text-sm rounded-lg transition-all font-sans"
+                >Documentation</a
+            >
+        </div>
+
+        <div
+            class="w-full max-w-[100px] py-2 px-1 flex justify-end items-center gap-1"
+        >
+        <a
+        href=""
+        class="hover:bg-zinc-200 dark:hover:bg-zinc-800 flex items-center px-4 py-2 text-sm rounded-lg transition-all font-sans font-medium"
+        >
+        
+        Login</a
+    >
+    
+        </div>
+        
+    </div>
+    
+</div>
+
+<div class="mx-auto relative w-full max-w-7xl h-[1px] bg-gradient-to-r from-transparent via-gray-500 to-transparent"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
 <script>
     import Navbar from "$lib/components/navbar.svelte";
 </script>
-<Navbar/>
+
+<div class="w-full">
+    <Navbar></Navbar>
+
+    <div class="w-full flex justify-center">
+        <div class="max-w-7xl">
+            
+        </div>
+    </div>
+</div>
